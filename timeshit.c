0f35ba1b54600c2467e309aefbe576faf2055194
rZwols <jlorenzo337x@gmail.com>
2026-06-19 23:42:19 +0200

    main changes

--- timeshit.c
+++ timeshit.c
@@ -1,6 +1,11 @@
 #include "timeshit.h"
 
 void init() {
+    initscr();
+    cbreak();
+    noecho();
+    nodelay(stdscr, TRUE);
+    refresh_current_time();
     /* initialize birthdate */
     database = fopen("database.txt", "r");
     if (database == NULL) {
@@ -8,21 +13,25 @@ void init() {
                 "   timeshit -s [yyyy/mm/dd]    \n");
         exit(EXIT_FAILURE);
     }
-    birthdate = malloc(sizeof(struct tm));
     int year;
     int month;
     int day;
     fscanf(database, "%d/%d/%d", &year, &month, &day);
+    birthdate = malloc(sizeof(struct tm));
+    *birthdate = emptyDate;
     birthdate->tm_year = year-1900;
     birthdate->tm_mon = month-1;
     birthdate->tm_mday = day;
-    /* */
-    initscr();
+    /* initialize endYear */
+    endYear = malloc(sizeof(struct tm));
+    *endYear = emptyDate;
+    endYear->tm_year = currentTime->tm_year+1;
     return;
 }
 
 void cleanup() {
     free(birthdate);
+    free(endYear);
     return;
 }
 
@@ -31,10 +40,49 @@ void refresh_current_time() {
     currentTime = gmtime(&rawCurrentTime);
 }
 
+void renderscr() {    
+    time_t rawCurrentTime = mktime(currentTime);
+    time_t rawBirthdate = mktime(birthdate);
+    time_t rawEndYear = mktime(endYear);
+
+    double lifetime = difftime(rawCurrentTime, rawBirthdate);
+
+    int totWeeks = lifetime / (7 * DAY_TO_SECOND_RATIO);
+
+    int years = lifetime / YEAR_TO_SECOND_RATIO;
+    lifetime -= years * YEAR_TO_SECOND_RATIO;
+    int days = lifetime / DAY_TO_SECOND_RATIO;
+    lifetime -= days * DAY_TO_SECOND_RATIO;
+    int hours = lifetime / HOUR_TO_SECOND_RATIO;
+    lifetime -= hours * HOUR_TO_SECOND_RATIO;
+    int minutes = lifetime / MINUTE_TO_SECOND_RATIO;
+    lifetime -= minutes * MINUTE_TO_SECOND_RATIO;
+    int seconds = lifetime;
+
+    int daysUntilEndYear = 1 + difftime(rawEndYear, rawCurrentTime) / DAY_TO_SECOND_RATIO;
+
+    printw("now: %04d/%02d/%02d %02d:%02d:%02d\n", 
+        currentTime->tm_year+1900, currentTime->tm_mon+1, currentTime->tm_mday,
+        currentTime->tm_hour, currentTime->tm_min, currentTime->tm_sec);
+    printw("you were born in %04d/%02d/%02d\n", 
+        birthdate->tm_year+1900, birthdate->tm_mon+1, birthdate->tm_mday);
+    printw("humans live on average 4000 weeks, you've already lived %d of yours\n",
+        totWeeks);
+    printw("days until the end of this year: %d\n", 
+        daysUntilEndYear);
+    printw("you're currently %dy, %dd, %02dh, %02dm, %02ds old\n", 
+        years, days, hours, minutes, seconds);
+}
+
 int main (int argc, char **argv) {
 
     atexit(cleanup);
 
+    struct timespec ts = {
+        .tv_sec = 0,
+        .tv_nsec = 500000000
+    };
+    
     int c;
     while ((c = getopt(argc, argv, "hs:")) != -1) {
         switch (c) {
@@ -55,7 +103,8 @@ int main (int argc, char **argv) {
             }
             database = fopen("database.txt", "w");
             if (database == NULL) {
-                perror("database.txt");
+                perror( "add your birthday with:        \n"
+                        "   timeshit -s [yyyy/mm/dd]    \n");
                 exit(EXIT_FAILURE);
             }
             fprintf(database, "%d/%d/%d", year, month, day);
@@ -72,13 +121,10 @@ int main (int argc, char **argv) {
     while (running) {
         refresh_current_time();
         clear();
-        printw("you were born in %d/%d/%d\n", 
-            birthdate->tm_year+1900, birthdate->tm_mon+1, birthdate->tm_mday);
-        printw("now: %d/%d/%d %d:%d:%d\n", 
-            currentTime->tm_year+1900, currentTime->tm_mon+1, currentTime->tm_mday,
-            currentTime->tm_hour, currentTime->tm_min, currentTime->tm_sec);
+        renderscr();
         refresh();
-        sleep(1);
+        nanosleep(&ts, NULL);
+        if (getch() == 'q') running = 0;
     }
 
     endwin();
--- timeshit.h
+++ timeshit.h
@@ -9,15 +9,31 @@
 #include <ncurses.h>
 #include <unistd.h>
 
+/* macros */
+#define MINUTE_TO_SECOND_RATIO  (60)
+#define HOUR_TO_SECOND_RATIO    (60*60)
+#define DAY_TO_SECOND_RATIO     (60*60*24)
+#define YEAR_TO_SECOND_RATIO    (60*60*24*356)
+
 /* prototypes */
 void init();
 void cleanup();
 void refresh_current_time();
+void renderscr();
 
 /* global variables */
 FILE *database;
+const struct tm emptyDate = {
+    .tm_year = 1900,
+    .tm_mon = 0,
+    .tm_mday = 1,
+    .tm_hour = 0,
+    .tm_min = 0,
+    .tm_sec = 0,
+};
 struct tm *birthdate;
 struct tm *currentTime;
+struct tm *endYear;
 int running;
 
 #endif /* TIMESHIT_H_INCLUDED */
