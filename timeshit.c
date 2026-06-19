2e63d6f41adc3ecb3849aa9b11664a1f64329b52
rZwols <jlorenzo337x@gmail.com>
2026-06-19 17:58:09 +0200

    base structure

--- timeshit.c
+++ timeshit.c
@@ -1,8 +1,52 @@
 #include "timeshit.h"
 
+void init() {
+    initscr();
+    return;
+}
+
+void cleanup() {
+    return;
+}
+
 int main (int argc, char **argv) {
 
-    initscr();
+    atexit(cleanup);
+
+    FILE *data;
+    int c;
+    while ((c = getopt(argc, argv, "hs:")) != -1) {
+        switch (c) {
+        case 'h':
+            printf(
+                "usage:                         \n"
+                "   -h      shows this message  \n"
+                "   -s      set your birthday   \n"
+            );
+            exit(EXIT_SUCCESS);
+            break;
+        case 's':
+            int year;
+            int month;
+            int day;
+            if (3 != sscanf(optarg, "%d/%d/%d", &year, &month, &day)) {
+                exit(EXIT_FAILURE);
+            }
+            data = fopen("database.txt", "w");
+            if (data == NULL) {
+                perror("database.txt");
+                exit(EXIT_FAILURE);
+            }
+            fprintf(data, "%d/%d/%d", year, month, day);
+            exit(EXIT_SUCCESS);
+            break;
+        default:
+
+            break;
+        }
+    }
+
+    init();
 
     getch();
 
--- timeshit.h
+++ timeshit.h
@@ -2,7 +2,13 @@
 #ifndef TIMESHIT_H_INCLUDED
 #define TIMESHIT_H_INCLUDED
 
+#include <assert.h>
+#include <stdlib.h>
 #include <time.h>
+#include <getopt.h>
 #include <ncurses.h>
+
+void init();
+void cleanup();
 
 #endif /* TIMESHIT_H_INCLUDED */
