1303ded995598862769044157cc82c4f5e6e8efc
rZwols <jlorenzo337x@gmail.com>
2026-06-19 20:12:58 +0200

    setup structure

--- timeshit.c
+++ timeshit.c
@@ -1,19 +1,40 @@
 #include "timeshit.h"
 
 void init() {
+    /* initialize birthdate */
+    database = fopen("database.txt", "r");
+    if (database == NULL) {
+        perror( "add your birthday with:        \n"
+                "   timeshit -s [yyyy/mm/dd]    \n");
+        exit(EXIT_FAILURE);
+    }
+    birthdate = malloc(sizeof(struct tm));
+    int year;
+    int month;
+    int day;
+    fscanf(database, "%d/%d/%d", &year, &month, &day);
+    birthdate->tm_year = year-1900;
+    birthdate->tm_mon = month-1;
+    birthdate->tm_mday = day;
+    /* */
     initscr();
     return;
 }
 
 void cleanup() {
+    free(birthdate);
     return;
+}
+
+void refresh_current_time() {
+    time_t rawCurrentTime = time(NULL);
+    currentTime = gmtime(&rawCurrentTime);
 }
 
 int main (int argc, char **argv) {
 
     atexit(cleanup);
 
-    FILE *data;
     int c;
     while ((c = getopt(argc, argv, "hs:")) != -1) {
         switch (c) {
@@ -32,23 +53,33 @@ int main (int argc, char **argv) {
             if (3 != sscanf(optarg, "%d/%d/%d", &year, &month, &day)) {
                 exit(EXIT_FAILURE);
             }
-            data = fopen("database.txt", "w");
-            if (data == NULL) {
+            database = fopen("database.txt", "w");
+            if (database == NULL) {
                 perror("database.txt");
                 exit(EXIT_FAILURE);
             }
-            fprintf(data, "%d/%d/%d", year, month, day);
+            fprintf(database, "%d/%d/%d", year, month, day);
             exit(EXIT_SUCCESS);
             break;
         default:
-
             break;
         }
     }
 
     init();
 
-    getch();
+    running = 1;
+    while (running) {
+        refresh_current_time();
+        clear();
+        printw("you were born in %d/%d/%d\n", 
+            birthdate->tm_year+1900, birthdate->tm_mon+1, birthdate->tm_mday);
+        printw("now: %d/%d/%d %d:%d:%d\n", 
+            currentTime->tm_year+1900, currentTime->tm_mon+1, currentTime->tm_mday,
+            currentTime->tm_hour, currentTime->tm_min, currentTime->tm_sec);
+        refresh();
+        sleep(1);
+    }
 
     endwin();
 }
--- timeshit.h
+++ timeshit.h
@@ -7,8 +7,17 @@
 #include <time.h>
 #include <getopt.h>
 #include <ncurses.h>
+#include <unistd.h>
 
+/* prototypes */
 void init();
 void cleanup();
+void refresh_current_time();
+
+/* global variables */
+FILE *database;
+struct tm *birthdate;
+struct tm *currentTime;
+int running;
 
 #endif /* TIMESHIT_H_INCLUDED */
